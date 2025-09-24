3a6060e3ae294c4ca02416a5a13bd20b8f6967ad
Jean Claude BIMENYIMANA <jeanclaudebimenyimana2023@gmail.com>
2025-09-24 20:35:04 +0100

    add logo in Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,19 @@
 import { Link } from "react-router-dom";
+import logo from "../assets/logo.png"; // make sure the path is correct
 
 export default function Navbar() {
   return (
     <nav className="bg-blue-600 text-white px-6 py-4 flex justify-between items-center">
-      <h1 className="text-xl font-bold">Wamba Computer Services</h1>
+      {/* Logo only */}
+      <Link to="/">
+        <img 
+          src={logo} 
+          alt="Wamba Computer Services Logo" 
+          className="h-10 w-auto" 
+        />
+      </Link>
+
+      {/* Navigation Links */}
       <ul className="flex space-x-6">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
